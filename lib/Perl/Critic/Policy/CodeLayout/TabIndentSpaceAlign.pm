3f35d8dc28665b3a18eced6b213df64c798be339
Guillaume Aubert <aubertg@cpan.org>
2012-09-23 00:31:31 -0400

    Released version 1.0.1.

--- lib/Perl/Critic/Policy/CodeLayout/TabIndentSpaceAlign.pm
+++ lib/Perl/Critic/Policy/CodeLayout/TabIndentSpaceAlign.pm
@@ -19,11 +19,11 @@ Perl::Critic::Policy::CodeLayout::TabIndentSpaceAlign - Use tabs for indenting, 
 
 =head1 VERSION
 
-Version 1.0.0
+Version 1.0.1
 
 =cut
 
-our $VERSION = '1.0.0';
+our $VERSION = '1.0.1';
 
 
 =head1 AFFILIATION
